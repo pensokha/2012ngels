66245f8c743c6bcd24921e6b40e4e3dd51be1859
7219kid <7219kid@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-10-09 11:53:42 +0000

    test

--- trunk/src/org/our/android/ouracademy/OurApplication.java
+++ trunk/src/org/our/android/ouracademy/OurApplication.java
@@ -3,10 +3,6 @@ package org.our.android.ouracademy;
 import org.our.android.ouracademy.util.DbManager;
 import org.our.android.ouracademy.util.ScreenInfo;
 
-import android.app.Application;
-import android.util.Log;
-
-
 /**
  * 
  * @author JiHoon, Moon on NTS
@@ -15,30 +11,31 @@ import android.util.Log;
 public class OurApplication extends Application {
 	private static boolean isTestMode = true;
 	private static OurApplication mOurApplication;
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
 		mOurApplication = this;
+		//ㅇㅇs
 
 		OurPreferenceManager.getInstance().initPreferenceData(this);
-		
+
 		//DbManager open
 		DbManager.getInstance().open(getApplicationContext());
-		
+
 		ScreenInfo.create(this);
 	}
-	
+
 	public static boolean isTestMode() {
 		return isTestMode;
 	}
-	
+
 	public static OurApplication getInstance() {
 		if (null == mOurApplication) {
 			Log.d("AeonApplication", "Application was killed!!!");
 		}
-		
+
 		return mOurApplication;
 	}
 }
