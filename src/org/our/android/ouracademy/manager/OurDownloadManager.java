d9ecd70990af0de300f69bbe1e107335a0edfd6a
7219kid <7219kid@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-10-10 01:35:37 +0000

    DownloadManager

---
+++ src/org/our/android/ouracademy/manager/OurDownloadManager.java
@@ -0,0 +1,73 @@
+package org.our.android.ouracademy.manager;
+
+import org.our.android.ouracademy.util.DbManager;
+import org.our.android.ouracademy.util.DbRow;
+
+import android.content.ContentValues;
+
+/**
+ * 
+ * @author jyeon
+ *
+ */
+public class OurDownloadManager {
+
+	public static final String Table_NAME = "download";
+
+	public static final String KEY_FILE_ID = "FileId";
+	public static final String KEY_DOWNLOAD_FILE_SIZE = "DownladedFileSize";
+	public static final String KEY_FILE_SIZE = "FileSize";
+	public static final String KEY_FILE_NAME = "FileName";
+	public static final String KEY_FILE_PATH = "FilePath";
+
+	public OurDownloadManager() {
+		// test code
+//		DbManager.getInstance().dropTable(tableName);
+
+		if (!DbManager.getInstance().isTableExist(Table_NAME)) {
+
+			ContentValues value = new ContentValues();
+			value.put(KEY_FILE_ID, "INTEGER");
+			value.put(KEY_DOWNLOAD_FILE_SIZE, "LONG");
+			value.put(KEY_FILE_SIZE, "LONG");
+			value.put(KEY_FILE_NAME, "TEXT");
+			value.put(KEY_FILE_PATH, "TEXT");
+
+			DbManager.getInstance().createTable(Table_NAME, value);
+		}
+
+//		updateStorage();
+	}
+
+	public void updateStorage() {
+		String[] list = FileManager.getList();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < list.length; i++) {
+			String fileName = list[i];
+			sb.setLength(0);
+			sb.append(FileManager.STRSAVEPATH);
+			sb.append(fileName);
+			addRow(-1, FileManager.geFileSize(sb.toString()), -1, fileName, sb.toString());
+		}
+	}
+
+	public void addRow(int id, long downladedFileSize, long fileSize, String fileName, String filePath) {
+		DbRow dbRow = new DbRow();
+		if (id != -1) {
+			dbRow.add(KEY_FILE_ID, id);
+		}
+		if (downladedFileSize != -1) {
+			dbRow.add(KEY_DOWNLOAD_FILE_SIZE, Long.toString(downladedFileSize));
+		}
+		if (fileSize != -1) {
+			dbRow.add(KEY_FILE_SIZE, Long.toString(fileSize));
+		}
+		if (fileName != null) {
+			dbRow.add(KEY_FILE_NAME, fileName);
+		}
+		if (filePath != null) {
+			dbRow.add(KEY_FILE_PATH, filePath);
+		}
+		DbManager.getInstance().insertOrReplace(Table_NAME, dbRow);
+	}
+}
