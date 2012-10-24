db437ad86638f33adf99729330cf2f9ad22ca90d
7219kid <7219kid@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-10-24 13:13:58 +0000

    make MediaPlayer UI

--- trunk/src/org/our/android/ouracademy/ui/view/MediaPlayerView.java
+++ trunk/src/org/our/android/ouracademy/ui/view/MediaPlayerView.java
@@ -3,26 +3,43 @@ package org.our.android.ouracademy.ui.view;
 import org.our.android.ouracademy.R;
 
 import android.content.Context;
-import android.media.MediaPlayer;
-import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MediaPlayerView extends LinearLayout implements SurfaceHolder.Callback {
+/**
+ * Media Player View
+ * 
+ * @author jyeon
+ *
+ */
+public class MediaPlayerView extends LinearLayout implements View.OnClickListener {
+
+	/**
+	 * 
+	 * @author Jyeon
+	 */
+	public interface OnMediaPlayerViewCb {
+		void onClickBtn(View view);
+	};
 
 	private SurfaceView preview;
-	private SurfaceHolder holder;
-	MediaPlayer player;
-	Button playBtn;
+
+	SeekBar progress;
+	VerticalSeekBar soundVolume;
+
+	ImageView playBtn, soundIcon;
+	LinearLayout closeBtn, soundBtn, moreBtn, list, soundControll;
+	LinearLayout frame;
 
 	TextView runTime, remainingTime;
+
+	OnMediaPlayerViewCb mediaPlayerViewCb;
 
 	public MediaPlayerView(Context context) {
 		super(context);
@@ -42,90 +59,155 @@ public class MediaPlayerView extends LinearLayout implements SurfaceHolder.Callb
 		li.inflate(R.layout.media_player, this, true);
 
 		preview = (SurfaceView)findViewById(R.id.surface);
-		holder = preview.getHolder();
-		holder.addCallback(this);
-		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		preview.setOnClickListener(this);
 
-		playBtn = (Button)findViewById(R.id.play);
-		playBtn.setOnClickListener(new OnClickListener() {
+		frame = (LinearLayout)findViewById(R.id.frame);
 
-			@Override
-			public void onClick(View v) {
-				if (player.isPlaying() == false) {
-					player.start();
-					playBtn.setText("Pause");
-				} else {
-					player.pause();
-					playBtn.setText("Play");
-				}
-			}
-		});
+		closeBtn = (LinearLayout)findViewById(R.id.closeBtn);
+		closeBtn.setOnClickListener(this);
+		playBtn = (ImageView)findViewById(R.id.play);
+		playBtn.setOnClickListener(this);
+		soundBtn = (LinearLayout)findViewById(R.id.soundBtn);
+		soundBtn.setOnClickListener(this);
+		moreBtn = (LinearLayout)findViewById(R.id.moreBtn);
+		moreBtn.setOnClickListener(this);
 
 		runTime = (TextView)findViewById(R.id.runTime);
 		remainingTime = (TextView)findViewById(R.id.remainingTime);
+
+		list = (LinearLayout)findViewById(R.id.list);
+		soundControll = (LinearLayout)findViewById(R.id.soundControll);
+
+		progress = (SeekBar)findViewById(R.id.progress);
+
+		soundVolume = (VerticalSeekBar)findViewById(R.id.soundVolume);
+		soundIcon = (ImageView)findViewById(R.id.soundIcon);
 	}
 
-	public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+	public SurfaceView getSurfaceView() {
+		return preview;
 	}
 
-	public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+	public SeekBar getSoundSeekBar() {
+		return soundVolume;
 	}
 
-	public void surfaceCreated(SurfaceHolder holder) {
-		if (player == null) {
-			player = new MediaPlayer();
-		} else {
-			player.reset();
-		}
-		try {
-			String sd = Environment.getExternalStorageDirectory().getAbsolutePath();
-			player.setDataSource(sd + "/OurAcademy/link_v03_9_24_2012_0.mp4");
-			player.setDisplay(holder);
-			player.prepare();
-			player.setOnCompletionListener(mComplete);
-			player.setOnVideoSizeChangedListener(mSizeChange);
-
-		} catch (Exception e) {
-			Toast.makeText(getContext(), "error : " + e.getMessage(),
-					Toast.LENGTH_LONG).show();
-		}
-
-		runTime.setText("00:00");
-		remainingTime.setText(getTimeText(player.getDuration()));
+	public SeekBar getVideoSeekBar() {
+		return progress;
 	}
 
-	MediaPlayer.OnCompletionListener mComplete =
-		new MediaPlayer.OnCompletionListener() {
-			public void onCompletion(MediaPlayer arg0) {
-				playBtn.setText("Play");
+	public void postShowFrame(final int show) {
+
+		postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				frame.setVisibility(show);
+
 			}
-		};
-
-	MediaPlayer.OnVideoSizeChangedListener mSizeChange =
-		new MediaPlayer.OnVideoSizeChangedListener() {
-			public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
-			}
-		};
-
-//	protected void onDestroy() {
-//		super.onDestroy();
-//		if (mPlayer != null) {
-//			mPlayer.release();
-//		}
-//	}
+		}, 3000);
+	}
 
 	public String getTimeText(int milliseconds) {
-
 		int second = milliseconds / 1000;
 		int minutes = second / 60;
 
 		StringBuilder sb = new StringBuilder();
+		if (minutes < 10) {
+			sb.append("0");
+		}
 		sb.append(minutes);
 		sb.append(":");
-		sb.append(second % 60);
+		int snd = second % 60;
+		if (snd < 10) {
+			sb.append("0");
+		}
+		sb.append(snd);
 
 //		Log.e("", "time = " + sb.toString());
 
 		return sb.toString();
 	}
+
+	@Override
+	public void onClick(View view) {
+
+		if (view.getId() != R.id.soundBtn) {
+			showSoundControll(false);
+		}
+
+		switch (view.getId()) {
+			case R.id.surface:
+				if (frame.getVisibility() == View.GONE) {
+					frame.setVisibility(View.VISIBLE);
+					showVideoList(false);
+				} else {
+					frame.setVisibility(View.GONE);
+				}
+				break;
+			case R.id.closeBtn:
+			case R.id.play:
+				if (mediaPlayerViewCb != null) {
+					mediaPlayerViewCb.onClickBtn(view);
+				}
+				break;
+			case R.id.soundBtn:
+				showSoundControll(!soundBtn.isSelected());
+				break;
+
+			case R.id.moreBtn:
+				showVideoList(!moreBtn.isSelected());
+				break;
+		}
+	}
+
+	private void showVideoList(boolean show) {
+		if (show) {
+			list.setVisibility(View.VISIBLE);
+		} else {
+			list.setVisibility(View.GONE);
+		}
+		moreBtn.setSelected(show);
+	}
+
+	private void showSoundControll(boolean show) {
+		if (show) {
+			soundControll.setVisibility(View.VISIBLE);
+			soundBtn.setSelected(true);
+		} else {
+			soundControll.setVisibility(View.GONE);
+			soundBtn.setSelected(false);
+		}
+	}
+
+	public void setSoundIconResource(int size) {
+		if (size == 0) {
+			soundIcon.setImageResource(R.drawable.player_icon_sound01);
+		} else if (size == soundVolume.getMax()) {
+			soundIcon.setImageResource(R.drawable.player_icon_sound02);
+		} else {
+			soundIcon.setImageResource(R.drawable.player_icon_sound03);
+		}
+	}
+
+	public void setPlayerResource(boolean play) {
+		if (play) {
+			playBtn.setBackgroundResource(R.drawable.btn_state_play_normal);
+		} else {
+			playBtn.setBackgroundResource(R.drawable.btn_state_play_stop);
+		}
+	}
+
+	public void setRunTimeText(int milliseconds) {
+		runTime.setText(getTimeText(milliseconds));
+	}
+
+	public void setRemainingTimeText(int milliseconds) {
+		remainingTime.setText(getTimeText(milliseconds));
+	}
+
+	public void setOnProgressBarClickCb(OnMediaPlayerViewCb callback) {
+		mediaPlayerViewCb = callback;
+	}
+
 }
