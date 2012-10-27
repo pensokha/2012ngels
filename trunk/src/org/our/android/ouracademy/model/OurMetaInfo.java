727593fa18edf9909e828bff2800cb072778f06d
back2moons <back2moons@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-10-27 13:46:01 +0000

    change package name

--- trunk/src/org/our/android/ouracademy/model/OurMetaInfo.java
+++ trunk/src/org/our/android/ouracademy/model/OurMetaInfo.java
@@ -6,8 +6,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
-
 public class OurMetaInfo implements OurJSONModel {
 	public static final int RES_CODE_SUCCESS = 0;
 	public static final int RES_CODE_DONT_NEED_UPDATE = -100;
