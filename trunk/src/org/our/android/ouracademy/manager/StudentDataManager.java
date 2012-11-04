4e3d214ff4b7173e6ce2cc3f399b422a08e86662
righths <righths@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-11-04 12:03:33 +0000

    다운로드 Queue 관리

--- trunk/src/org/our/android/ouracademy/manager/StudentDataManager.java
+++ trunk/src/org/our/android/ouracademy/manager/StudentDataManager.java
@@ -1,5 +1,6 @@
 package org.our.android.ouracademy.manager;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.our.android.ouracademy.asynctask.CallbackTask;
@@ -9,6 +10,7 @@ import org.our.android.ouracademy.model.OurContents;
 import org.our.android.ouracademy.p2p.client.DownloadClient;
 import org.our.android.ouracademy.p2p.client.GetMetaInfoClient;
 import org.our.android.ouracademy.service.StudentService;
+import org.our.android.ouracademy.ui.pages.MainActivity.OurDataChangeReceiver;
 import org.our.android.ouracademy.util.NetworkState;
 import org.our.android.ouracademy.wifidirect.WifiDirectWrapper;
 
@@ -16,8 +18,8 @@ import android.content.Context;
 import android.content.Intent;
 
 public class StudentDataManager extends DataManager {
-	private static HashMap<String, ExecutorPair> downloadMap = new HashMap<String, ExecutorPair>();
 	private static StudentDataManager instance = new StudentDataManager();
+	private DownloadManager downloadManager = new DownloadManager();
 
 	public static DataManager getInstance() {
 		return instance;
@@ -77,43 +79,109 @@ public class StudentDataManager extends DataManager {
 	@Override
 	public void download(OurContents content) {
 		if (NetworkState.isWifiDirectConnected()) {
-			CallbackTask downloadTask = new DownloadClient(WifiDirectWrapper
-					.getInstance().getOwnerIP(), content, context);
-			downloadTask.addCallback(new ExecutorCallbackTask(content));
-			ExecutorPair pair = executeRunnable(downloadTask);
-
-			synchronized (downloadMap) {
-				downloadMap.put(content.getId(), pair);
-			}
+			downloadManager.addContent(content);
+			downloadManager.executeDownload();
 		}
 	}
 
 	@Override
 	public void cancelDownload(OurContents content) {
-		synchronized (downloadMap) {
-			ExecutorPair pair = downloadMap.get(content.getId());
-			if (pair != null) {
-				if (pair.future.isDone()) {
-					downloadMap.remove(content.getId());
-				} else {
-					pair.executor.shutdownNow();
+		downloadManager.cancelDownload(content);
+	}
+
+	private class DownloadManager {
+		private HashMap<String, OurContents> downloadCotents;
+		private ArrayList<String> contentIdList;
+		private ExecutorPair pair;
+
+		public DownloadManager() {
+			super();
+			pair = null;
+			contentIdList = new ArrayList<String>();
+			downloadCotents = new HashMap<String, OurContents>();
+		}
+
+		public synchronized void addContent(OurContents content) {
+			if (content != null) {
+				if (downloadCotents.containsKey(content.getId()) == false) {
+					contentIdList.add(content.getId());
+					downloadCotents.put(content.getId(), content);
 				}
 			}
 		}
-	}
 
-	private class ExecutorCallbackTask implements OurCallback {
-		private OurContents content;
+		public synchronized void cancelDownload(OurContents content) {
+			if (contentIdList.size() > 0 && content != null) {
+				if (contentIdList.get(0).equals(content.getId())) {
+					if (pair != null) {
+						if (pair.future.isDone()) {
+							pair = null;
 
-		public ExecutorCallbackTask(OurContents content) {
-			super();
-			this.content = content;
+							downloadCotents.remove(content.getId());
+							contentIdList.remove(0);
+
+							notiCancel(content.getId());
+						} else {
+							pair.executor.shutdownNow();
+						}
+					}
+				} else {
+					downloadCotents.remove(content.getId());
+					contentIdList.remove(content.getId());
+
+					notiCancel(content.getId());
+				}
+			}
 		}
 
-		@Override
-		public void callback() {
-			synchronized (downloadMap) {
-				downloadMap.remove(content.getId());
+		public void notiCancel(String contentId) {
+			Intent intent = new Intent(OurDataChangeReceiver.OUR_DATA_CHANGED);
+			intent.putExtra(OurDataChangeReceiver.ACTION,
+					OurDataChangeReceiver.ACTION_CANCEL_DOWNLOADING);
+			intent.putExtra(OurDataChangeReceiver.CONTENT_ID, contentId);
+
+			context.sendBroadcast(intent);
+		}
+
+		public synchronized void executeDownload() {
+			if (pair == null && contentIdList.size() > 0) {
+				String contentId = contentIdList.get(0);
+				OurContents content = downloadCotents.get(contentId);
+				if (content != null) {
+					CallbackTask downloadTask = new DownloadClient(
+							WifiDirectWrapper.getInstance().getOwnerIP(),
+							content, context);
+					downloadTask.addCallback(new ExecutorCallbackTask(content));
+					pair = executeRunnable(downloadTask);
+				}else{
+					contentIdList.remove(0);
+				}
+			}
+		}
+		
+		public synchronized void endDownload(OurContents content){
+			if(contentIdList.size() > 0){
+				contentIdList.remove(content.getId());
+				downloadCotents.remove(content.getId());
+			}
+			pair = null;
+			
+			if (NetworkState.isWifiDirectConnected()) {
+				executeDownload();
+			}
+		}
+		
+		private class ExecutorCallbackTask implements OurCallback {
+			private OurContents content;
+
+			public ExecutorCallbackTask(OurContents content) {
+				super();
+				this.content = content;
+			}
+
+			@Override
+			public void callback() {
+				endDownload(content);
 			}
 		}
 	}
