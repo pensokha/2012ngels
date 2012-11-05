3ffef4c04bcd956d146daae93dfe58f1b2858324
righths <righths@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-11-05 15:04:23 +0000

    Service Mode 정리

--- src/org/our/android/ouracademy/service/StudentService.java
+++ src/org/our/android/ouracademy/service/StudentService.java
@@ -1,8 +1,18 @@
 package org.our.android.ouracademy.service;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 
 public class StudentService extends OurService {
+
+	@Override
+	public void onCreate() {
+		WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		wifi.disconnect();
+		
+		super.onCreate();
+	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
--- src/org/our/android/ouracademy/wifidirect/WifiDirectDefaultListener.java
+++ src/org/our/android/ouracademy/wifidirect/WifiDirectDefaultListener.java
@@ -1,13 +1,8 @@
 package org.our.android.ouracademy.wifidirect;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
-import android.provider.Settings;
 import android.util.Log;
 
 /**********
@@ -15,10 +10,8 @@ import android.util.Log;
  * @author hyeongseokLim
  * 
  */
-public abstract class WifiDirectDefaultListener implements WifiDirectListener,
-		OnClickListener {
+public abstract class WifiDirectDefaultListener implements WifiDirectListener {
 	private boolean connnect = false;
-	private boolean dialogOn = true;
 	protected WifiP2pManager manager;
 	protected Channel channel;
 	protected Context context;
@@ -34,21 +27,6 @@ public abstract class WifiDirectDefaultListener implements WifiDirectListener,
 	@Override
 	public void onDisableP2p() {
 		Log.d("Test", "onDisableP2p");
-		if (dialogOn == true) {
-			new AlertDialog.Builder(context)
-					.setTitle("Do you want to set wifidirect?")
-					.setPositiveButton("yes", this)
-					.setNegativeButton("no", this).show();
-		}
-	}
-
-	@Override
-	public void onClick(DialogInterface dialog, int which) {
-		if (which == -1) {
-			context.startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
-		} else {
-			dialogOn = false;
-		}
 	}
 
 	@Override
