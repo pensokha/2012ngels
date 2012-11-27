c5a0b7827f6f9bc28d60d7b41343c57d672c15f0
7219kid <7219kid@8708d578-1b31-54f7-7dc8-71cd2b3b7a9b>
2012-11-27 16:02:04 +0000

    player list logic

--- trunk/src/org/our/android/ouracademy/ui/view/PlayerListItemView.java
+++ trunk/src/org/our/android/ouracademy/ui/view/PlayerListItemView.java
@@ -1,22 +1,42 @@
 package org.our.android.ouracademy.ui.view;
 
+import java.io.File;
+
 import org.our.android.ouracademy.R;
+import org.our.android.ouracademy.constants.CommonConstants;
+import org.our.android.ouracademy.constants.MatchCategoryColor;
+import org.our.android.ouracademy.model.OurCategory;
+import org.our.android.ouracademy.model.OurContents;
 import org.our.android.ouracademy.ui.widget.VerticalTextView;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class PlayerListItemView extends LinearLayout {
+public class PlayerListItemView extends LinearLayout implements OnClickListener {
 
+	public interface PlayerListItemViewListener {
+		void onClickBook(OurContents contents);
+	}
+
+	OurContents contents;
+
+	RelativeLayout book;
 	VerticalTextView subject;
 	TextView title;
 	ImageView thumbnail;
+
+	private PlayerListItemViewListener listener;
 
 	public PlayerListItemView(Context context) {
 		super(context);
@@ -35,17 +55,31 @@ public class PlayerListItemView extends LinearLayout {
 		LayoutInflater li = (LayoutInflater)getContext().getSystemService(infService);
 		li.inflate(R.layout.player_list_item, this, true);
 
+		book = (RelativeLayout)findViewById(R.id.book);
 		subject = (VerticalTextView)findViewById(R.id.subject);
 		title = (TextView)findViewById(R.id.title);
-		thumbnail = (ImageView)findViewById(R.id.thumbnail);
+		thumbnail = (ImageView)findViewById(R.id.thumb);
+
+		this.setOnClickListener(this);
 	}
 
-	public void setData(String subject, String title, Drawable imageDrawable) {
-		setSubjectText(subject);
-		setTitleText(title);
+	public void setData(OurContents contents) {
+		if (contents != null) {
+			this.contents = contents;
 
-		if (imageDrawable != null) {
-			setThumbnailImage(imageDrawable);
+			OurCategory category = contents.selectedCategory;
+			if (category != null) {
+				setSubjectText(category.getCategoryTitle());
+				setBookBackgroundResource(MatchCategoryColor.getCategoryMatchColorId(contents.selectedCategory == null
+					? "" : contents.selectedCategory.getCategoryId()));
+			}
+
+			setTitleText(contents.getSubject());
+
+			final String existingFile = CommonConstants.getContentImagePath(contents.getId());
+			if (new File(existingFile).exists()) {
+				setThumbnailImage(BitmapFactory.decodeFile(existingFile));
+			}
 		}
 	}
 
@@ -63,5 +97,29 @@ public class PlayerListItemView extends LinearLayout {
 
 	public void setThumbnailImage(Drawable imageDrawable) {
 		thumbnail.setBackground(imageDrawable);
+	}
+
+	public void setThumbnailImage(Bitmap bm) {
+		if (thumbnail != null && bm != null) {
+			thumbnail.setImageBitmap(bm);
+		}
+	}
+
+	public void setBookBackgroundResource(int resid) {
+		if (book != null) {
+			book.setBackgroundResource(resid);
+		}
+	}
+
+	public void setOnPlayerListViewListener(PlayerListItemViewListener listener) {
+		this.listener = listener;
+	}
+
+	@Override
+	public void onClick(View v) {
+		int h = getHeight();
+		if (listener != null) {
+			listener.onClickBook(contents);
+		}
 	}
 }
